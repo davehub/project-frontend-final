1977a1efb0bc746416cad57e6efb7484e992c724
dave <dave.developpeur.dave@gmail.com>
2025-06-14 05:34:28 +0000

    update files

--- src/pages/CreateEquipmentPage.tsx
+++ src/pages/CreateEquipmentPage.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import 'material-icons/iconfont/material-icons.css';
 
 interface IUser {
   _id: string;
